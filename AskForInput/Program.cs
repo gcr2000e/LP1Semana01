4698fd299eacda889a3e92a6fdb0b20ce7e4c31e
gcr2000e <a22409619@alunos.ulht.pt>
2025-02-19 19:08:26 +0000

    Add AskForInput

---
+++ AskForInput/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        float soma;
+        private void Main(string[] args)
+        {
+            Console.WriteLine("Insira um número inteiro:");
+            string str = Console.ReadLine();
+            int i = int.Parse(str);
+            
+            Console.WriteLine("Insire um número real");
+            string str2 = Console.ReadLine();
+            float j = float.Parse(str2);
+
+            soma = i + j;
+            Console.WriteLine($"A soma é {soma:f2}");
+        }
+    }
+}
