b6c25685e962e307482aa924203fe0f02a2e2611
gcr2000e <a22409619@alunos.ulht.pt>
2025-02-19 18:40:11 +0000

    Add ManyStrings project

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        string m = "Olá mundo";
+        string s = "Um tab\t e um copyright \u03A9";
+        string path = "C:\\Users\\Gonçalo\\Desktop";
+        private void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(m);
+            Console.WriteLine(s);
+            Console.WriteLine(path);
+        }
+    }
+}
