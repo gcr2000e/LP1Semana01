e8c0ef4d0f5de7230b3f1412c5408fd643e010e9
gcr2000e <a22409619@alunos.ulht.pt>
2025-02-19 17:39:19 +0000

    Add comments on Program.cs code

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Classe do programa "MeuPrograma"
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Imprime Hello LP1! e Bye World
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
